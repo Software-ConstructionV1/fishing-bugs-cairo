106c1077a78440adf703fcaa8d8986bbcda59600
ahmedoweis72 <ar809958@gmail.com>
2025-05-16 16:09:43 +0300

    Refactor application structure and update routes
    - Un-commented source line in Gemfile.
    - Updated ApplicationController to inherit from ActionController::Base.
    - Changed UsersController method from show to index.
    - Deleted unused index view for users.
    - Updated routes to correctly map users to UsersController#index.
    - Created User model file.
    - Added index view for users.

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/users', to: 'user#index'
+  # get '/users', to: 'user#index'
+    get "users" => "users#index"
+
 end
